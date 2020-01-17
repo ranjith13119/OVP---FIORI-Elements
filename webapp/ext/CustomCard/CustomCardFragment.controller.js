554455fedb7fc03db43488c376175ea19846eedb
Ranjithkumar Ayyavu <ranjith13119@gmail.com>
2020-01-17 18:10:42 +0000

    My First Push

---
+++ webapp/ext/CustomCard/CustomCardFragment.controller.js
@@ -0,0 +1,21 @@
+(function () {
+	"use strict";
+
+	/* controller for custom card  */
+
+	sap.ui.controller("OVP.OverViewPage.ext.CustomCard.CustomCardFragment", {
+
+		onInit: function () {
+
+		},
+
+		onAfterRendering: function () {
+
+		},
+
+		onExit: function () {
+
+		}
+
+	});
+})();
